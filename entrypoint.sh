f8ef078cfd92a68652bb663ded2079799cba5070
Andreas Oeldemann <hey@aoel.io>
2019-01-19 02:17:51 +0100

    initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+curl --silent https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts | \
+  grep -v "^0\.0\.0\.0 0\.0\.0\.0$" | grep '^0\.0\.0\.0' | \
+  awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' \
+  > /etc/unbound/blacklist
+exec unbound -c /etc/unbound/unbound.conf
