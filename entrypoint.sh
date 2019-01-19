f7bbf52be511f8c4639c3faa94213cdf407008f9
Andreas Oeldemann <hey@aoel.io>
2019-01-19 02:40:11 +0100

    added github ip to hosts file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo "151.101.112.133 raw.githubusercontent.com" >> /etc/hosts
 curl --silent https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts | \
   grep -v "^0\.0\.0\.0 0\.0\.0\.0$" | grep '^0\.0\.0\.0' | \
   awk '{print "local-zone: \""$2"\" redirect\nlocal-data: \""$2" A 0.0.0.0\""}' \
